2122148a5a6e0712ef42f21a610914c4166d6c6c
Gerson Bergeth <gerson.bergeth@nrc.no>
2026-07-24 18:19:55 +0300

    Refine Street Art navigation and gallery viewer

---
+++ src/data/streetArt.js
@@ -0,0 +1,72 @@
+export const streetArt = [
+  {
+    id: 1,
+    video: "/videos/street/street-art-001.mp4",
+    city: "Berlin",
+    country: "Germany",
+    artist: "Unknown"
+  },
+  {
+    id: 2,
+    video: "/videos/street/street-art-002.mp4",
+    city: "Valencia",
+    country: "Spain",
+    artist: "Olivier Secretan (OSE)"
+  },
+  {
+    id: 3,
+    video: "/videos/street/street-art-003.mp4",
+    city: "Nairobi",
+    country: "Kenya",
+    artist: "Erick Stickky Muriithi (stic_oner)"
+  },
+  {
+    id: 4,
+    video: "/videos/street/street-art-004.mp4",
+    city: "Los Angeles",
+    country: "United States",
+    artist: "Royyal Dog (Chris Chanyang Shim)"
+  },
+  {
+    id: 5,
+    video: "/videos/street/street-art-005.mp4",
+    city: "Wrocław",
+    country: "Poland",
+    artist: "Mariusz Mikołajek, Witold Liszkowski, Jan Mikołajek"
+  },
+  {
+    id: 6,
+    video: "/videos/street/street-art-006.mp4",
+    city: "Ibiza",
+    country: "Spain",
+    artist: "ShenShen210"
+  },
+  {
+    id: 7,
+    video: "/videos/street/street-art-007.mp4",
+    city: "Kyiv",
+    country: "Ukraine",
+    artist: "Slavomir Czajkowski"
+  },
+  {
+    id: 8,
+    video: "/videos/street/street-art-008.mp4",
+    city: "Oslo",
+    country: "Norway",
+    artist: "Ohno @ohno.sts.csm "
+  },
+  {
+    id: 9,
+    video: "/videos/street/street-art-009.mp4",
+    city: "Sarajevo",
+    country: "Bosnia and Herzegovina",
+    artist: "Unknown"
+  },
+  {
+    id: 10,
+    video: "/videos/street/street-art-010.mp4",
+    city: "Wrocław",
+    country: "Poland",
+    artist: "OKAP (Ośrodek Kulturalnej Animacji Podwórkowej)"
+  }
+];
